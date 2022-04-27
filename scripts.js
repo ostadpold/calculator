22431df0095230d6a925b81710a940cabf6aee85
ostadnavid <97975161+ostadnavid@users.noreply.github.com>
2022-04-27 23:49:51 +0430

    update

--- scripts.js
+++ scripts.js
@@ -1,76 +1,64 @@
 let myHiddenline = document.querySelector('.hidden-arguments');
 let myLine = document.querySelector('.container-arguments');
-
 let myDisplay = document.querySelector('.container-display input');
 let myNumbersbutton = document.querySelectorAll('.numbersbutton');//collection
 let myNumbers = document.querySelectorAll('.number');//collection
 let myNumbersvalue;
 let operators = document.querySelectorAll('.operator');
 let differntoperators = document.querySelectorAll('.different');
-
 let mycalculation = "";
 
 myDisplay.setAttribute("value","");
 
+for(let i = 0;i <4;i++){
+   let operatorsvalue = ["/","*","-","+"];//for operators
+   operators[i].setAttribute("value",operatorsvalue[i]);
+}
+for(let i = 0;i <= 10;i++){//for numbers value
+   let myNumbersvalue =  myNumbers[i].innerHTML;
+   myNumbersbutton[i].setAttribute("value",myNumbersvalue);
+}
 function togglehiddenline(){
    myHiddenline.classList.toggle("active");
    myLine.classList.toggle('active');
-}
-for(let i = 0;i<2;i++){
-   let operatorsvalue = ["&#247;","&#215;"];
-   differntoperators[i].setAttribute("Display",operatorsvalue[i]);
-   // console.log(differntoperators[i]);
-}
-
-for(let i = 0;i <4;i++){
-   let operatorsvalue = ["/","*","-","+"];
-   operators[i].setAttribute("value",operatorsvalue[i]);
-}
-
-for(let i = 0;i <= 10;i++){
-   let myNumbersvalue =  myNumbers[i].innerHTML;
-   myNumbersbutton[i].setAttribute("value",myNumbersvalue);
 }
 function Numbers(event){
    let value = event.target.value;
 
    mycalculation += value;
-   myDisplay.value = mycalculation;
+   myDisplay.value += value;
 
    console.log(mycalculation);
 }
 function clearDisplay(){
    mycalculation = "";
-   myDisplay.value = mycalculation;
+   myDisplay.value = "";
 
    console.log(mycalculation);
 }
 function DeleteDisplay(){
    mycalculation = mycalculation.slice(0,-1);
-   myDisplay.value = mycalculation;
+   myDisplay.value =  myDisplay.value.slice(0,-1);
 
    console.log(mycalculation);
 }
 function percentDisplay(){
    mycalculation = mycalculation / "100" + "";
+   myDisplay.value = myDisplay.value / "100"+ "";
    if(mycalculation == "NaN"){
       mycalculation = "";
-      myDisplay.value = "ERROR";
-   }else {
-      myDisplay.value = mycalculation;
+      myDisplay.value = "";
    }
-
    console.log(mycalculation);
 }
 function operator(event){
-   // if(event.target.classList.contains("different")){
-   //    mycalculation += event.target.value;
-   //    myDisplay.value += event.target.attributes[2].value;
-   // }
+   if(event.target.classList.contains("different")){
       mycalculation += event.target.value;
-      myDisplay.value = mycalculation;
-
-
+      myDisplay.value += event.target.attributes[0].value;
+   }else {
+      mycalculation += event.target.value;
+      myDisplay.value += event.target.value;
+   }
    console.log(mycalculation);
 }
 function equals(){
