ecfbd619dcb0f86d7d330140a14a4ceb72383bf6
ostadnavid <97975161+ostadnavid@users.noreply.github.com>
2022-04-29 13:20:01 +0430

    np

--- scripts.js
+++ scripts.js
@@ -1,20 +1,16 @@
 let myHiddenline = document.querySelector('.hidden-arguments');
-let myLine = document.querySelector('.container-arguments');
 let myDisplay = document.querySelector('.container-display input');
 let myNumbersbutton = document.querySelectorAll('.numbersbutton');//collection
 let myNumbers = document.querySelectorAll('.number');//collection
 let myNumbersvalue;
 let operators = document.querySelectorAll('.operator');
-let differntoperators = document.querySelectorAll('.different');
-let mycalculation = "";
-let mybox = document.querySelector('.box');
+let mycalculation='';
 let HTML = document.querySelector('html');
-myDisplay.setAttribute("value","");
+let mysecondDisplay = document.querySelector('.container-display input:last-child');
+let myDivdisplay = document.querySelector('.container-display');
+let boxdisplay = document.querySelector('.boxdisplay');
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-   // const newColorScheme = event.matches ? "dark" : "light";
-   alert(12);
-});
+myDisplay.setAttribute("value","");
 
 for(let i = 0;i <4;i++){
    let operatorsvalue = ["/","*","-","+"];//for operators
@@ -24,6 +20,9 @@ for(let i = 0;i <= 10;i++){//for numbers value
    let myNumbersvalue =  myNumbers[i].innerHTML;
    myNumbersbutton[i].setAttribute("value",myNumbersvalue);
 }
+function clickedtoggle() {
+   boxdisplay.classList.toggle("active");
+}
 function togglehiddenline(){
    myHiddenline.classList.toggle("active");
 }
@@ -32,46 +31,60 @@ function togglebox(){
 }
 function Numbers(event){
    let value = event.target.value;
+   myDivdisplay.classList.remove("active");
+   myDivdisplay.classList.add("visible");
 
    mycalculation += value;
    myDisplay.value += value;
+   mysecondDisplay.value = eval(mycalculation);
 
    console.log(mycalculation);
+   
 }
 function clearDisplay(){
    mycalculation = "";
    myDisplay.value = "";
-
+   mysecondDisplay.value = mycalculation;
+   myDivdisplay.classList.remove("active");
+   myDivdisplay.classList.remove("visible");
+   
    console.log(mycalculation);
 }
 function DeleteDisplay(){
    mycalculation = mycalculation.slice(0,-1);
    myDisplay.value =  myDisplay.value.slice(0,-1);
-
+   mysecondDisplay.value = eval(mycalculation);
+   myDivdisplay.classList.remove("active");
+   if(myDisplay.value == ""){
+      myDivdisplay.classList.remove("visible");
+   }
+   if(mysecondDisplay.value == "undefined"){
+      mysecondDisplay.value = "";
+   }
+   
    console.log(mycalculation);
 }
 function percentDisplay(){
-   mycalculation = mycalculation / "100" + "";
-   myDisplay.value = myDisplay.value / "100"+ "";
-   if(mycalculation == "NaN"){
-      mycalculation = "";
-      myDisplay.value = "";
-   }
+   mycalculation = mycalculation / 100;
+   myDisplay.value = mycalculation;
+   mysecondDisplay.value = mycalculation;
+   myDivdisplay.classList.remove("active");
+
    console.log(mycalculation);
+   
 }
 function operator(event){
-   if(event.target.classList.contains("different")){
-      mycalculation += event.target.value;
-      myDisplay.value += event.target.attributes[0].value;
-   }else {
-      mycalculation += event.target.value;
-      myDisplay.value += event.target.value;
-   }
+   mycalculation += event.target.value;
+   myDisplay.value += event.target.attributes[0].value;
+   myDivdisplay.classList.remove("active");
+   
    console.log(mycalculation);
+   
 }
 function equals(){
-   mycalculation = eval(mycalculation).toString();
-   myDisplay.value = mycalculation;
+   mycalculation = mycalculation.toString();
+   mysecondDisplay.value = eval(mycalculation);
+   myDivdisplay.classList.add("active");
 
    console.log(mycalculation);
 }
